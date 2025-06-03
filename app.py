cd80d45a41147dcfcd30f125440f067e0faa0ff0
Rafael Aryapati Soebagijo <rafael.aryapati224@gmail.com>
2025-06-03 20:45:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def load_lstm_model():
 @st.cache_resource(show_spinner=False)
 def load_rf_model():
     try:
-        model = joblib.load("rf_sentiment_model.pkl")
+        model = joblib.load("best_rf_sentiment_model.pkl")
         vectorizer = joblib.load("tfidf_vectorizer.pkl")
         assert hasattr(vectorizer, "idf_"), "Vectorizer belum fit!"
         return model, vectorizer
