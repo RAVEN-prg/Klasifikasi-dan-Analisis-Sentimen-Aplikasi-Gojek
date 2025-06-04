e549fa7aaa5c23f3750431cb7ddf609f7ab2d925
Rafael Aryapati Soebagijo <rafael.aryapati224@gmail.com>
2025-06-05 01:05:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ labels = ["Negatif", "Netral", "Positif"]
 
 @st.cache_resource(show_spinner=False)
 def load_lstm_model():
-    model = tf.keras.models.load_model("lstm_sentiment_model.h5")
+    model = tf.keras.models.load_model("model/lstm_sentiment_model.h5")
     tokenizer = load_tokenizer("tokenizer.pickle")
     return model, tokenizer
 
